409d837e66f734b7a3447454008657d129925659
sebyta0 <szavala529@gmail.com>
2026-04-06 14:42:02 -0400

    Refactor contar_ciclo and contar_recursivo functions
    Fix indentation and correct function call in recursive method.

--- Semana5/ejercicio1.py
+++ Semana5/ejercicio1.py
@@ -1,25 +1,18 @@
-"""
-Ejercicio 1: Dado un número entero positivo N, retornar una lista con los números desde 1 hasta N.
-
-Debe implementar dos funciones:
-1. Una usando iteración (for o while)
-2. Una usando recursividad
-"""
-
+n = int(input("Ingrese numero: "))
 def contar_ciclo(n):
     resultado = []
     for i in range (1 , n + 1):
         resultado.append(i)
-        return (resultado)
+    return (resultado)
         
 def contar_recursivo(n):
     if n == 1:
         return[1]
     else:
-        lista = list_recur (n - 1)
+        lista = contar_recursivo(n - 1)
         lista.append(n)
         return lista 
-        
+
 print(f"Resultado ciclo: {contar_ciclo(n)}")
 print(f"Resultado recursivo: {contar_recursivo(n)}")
 
